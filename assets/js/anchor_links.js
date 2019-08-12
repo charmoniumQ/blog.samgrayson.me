b294ab6610c08bc952293305e263caa6e34da6b6
Samuel Grayson <sam@samgrayson.me>
2019-08-12 12:46:19 -0700

    generated 2019-46-12 from https://github.com/charmoniumQ/blog-content/commit/0ada3bbf51ab7a9b347da124001d755312afa441

--- assets/js/anchor_links.js
+++ assets/js/anchor_links.js
@@ -1,12 +1,1 @@
-// https://ben.balter.com/2014/03/13/pages-anchor-links/#the-javascript
-$(document).ready(function() {
-  return $("h2, h3, h4, h5, h6").each(function(i, el) {
-    var $el, icon, id;
-    $el = $(el);
-    id = $el.attr('id');
-    icon = '<i class="fa fa-link"></i>';
-    if (id) {
-      return $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
-    }
-  });
-});
+$(document).ready(function(){return $("h2, h3, h4, h5, h6").each(function(a,r){var e,n,t;if(n='<i class="fa fa-link"></i>',t=(e=$(r)).attr("id"))return e.prepend($("<a />").addClass("header-link").attr("href","#"+t).html(n))})});
