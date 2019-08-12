33ee412cb935749b670062a49dd391895ec054b5
Samuel Grayson <sam@samgrayson.me>
2019-08-12 13:14:56 -0700

    generated 2019-14-12 from https://github.com/charmoniumQ/blog-content/commit/9a8dba9aef1b6258b3f146e8811037fa9995c19b

--- assets/js/spoilers.js
+++ assets/js/spoilers.js
@@ -1,1 +1,38 @@
-function setVisibility(i,t){const e=$("button",i),d=i.children().not("button").first();t?(i.data("hidden",!0),e.text("Click to hide"),d.show()):(i.data("hidden",!1),e.text("Click to show"),d.hide())}function toggleVisibility(i){setVisibility(i,!i.data("hidden"))}$(document).ready(function(){$(".spoiler").each((i,t)=>{$div=$(t),setVisibility($div,!1),$("button",$div).first().click(i=>toggleVisibility($(i.target).parent()))})});
+function setVisibility($div, visibility)  {
+  const $button = $("button", $div);
+  const $text = $div.children().not("button").first();
+  if (visibility) {
+    $div.data('hidden', true);
+    $button.text('Click to hide');
+    $text.show();
+  } else {
+    $div.data('hidden', false);
+    $button.text('Click to show');
+    $text.hide();
+  }
+}
+
+function toggleVisibility($div) {
+  setVisibility($div, !$div.data('hidden'));
+}
+
+$(document).ready(function() {
+  $('.spoiler').each((_, div) => {
+    $div = $(div);
+
+    // set initial state
+    setVisibility($div, false);
+
+    // set onClick handler
+    $("button", $div).first().click((event) => toggleVisibility($(event.target).parent()));
+  });
+});
+
+/*
+spoilers have signature:
+<container class="spoiler">
+  <content></content>
+  <button></button>
+</container>
+*/
+
